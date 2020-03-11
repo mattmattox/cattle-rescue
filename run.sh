153af320d554af6e4abd2c684a8b246fd4db1763
Matthew Mattox <matthew.mattox@rancher.com>
2020-03-11 03:15:02 -0500

    Testing soft failover

--- run.sh
+++ run.sh
@@ -70,27 +70,11 @@ function check_cluster_health () {
   kubectl --kubeconfig /tmp/"$pair"/"$1"/kube_config_cluster.yml get nodes
 }
 
-function soft_cluster_failover () {
-  DATE=`date +%s`
-  SourceCluster=$1
-  TargetCluster=$2
-  echo "Starting soft failover from $SourceCluster to $TargetCluster"
-  cd /tmp/"$pair"/"$SourceCluster"/
-  echo "Setting SSH access..."
-  SSH_USER=`cat ./ssh-user`
-  unlink /root/.ssh/id_rsa
-  ln -s ./ssh-key /root/.ssh/id_rsa
-  echo "Taking snapshot of $SourceCluster"
-  snapshot_name=`echo CattleRescue-"$DATE"`
-  rke etcd snapshot-save --name "$snapshot_name"
-  echo "Shutting down docker on $SourceCluster"
-  for node in $(cat cluster.yml | grep ' address:' | awk '{print $3}')
-  do
-    ssh "$SSH_USER"@$node "systemctl disable docker; systemctl stop docker"
-  done
-  echo "Calling Flip Provider..."
-  flip_dns $SourceCluster $TargetCluster
-  echo "Bring up $TargetCluster"
+function bring_up_target_cluster () {
+  pair=$1
+  SourceCluster=$2
+  TargetCluster=$3
+  snapshot_name=$4
   cd /tmp/"$pair"/"$TargetCluster"/
   echo "Setting SSH access..."
   SSH_USER=`cat ./ssh-user`
@@ -120,7 +104,139 @@ function soft_cluster_failover () {
   echo "Staring etcd restore..."
   rke etcd snapshot-restore --name "$snapshot_name"
 
+  echo "Fixing tokens..."
+  for namespace in kube-system cattle-system ingress-nginx
+  do
+    echo "namespace: $namespace"
+    for token in $(kubectl --kubeconfig kube_config_cluster.yml get secret -n $namespace | grep 'kubernetes.io/service-account-token' | awk '{print $1}')
+    do
+      kubectl --kubeconfig kube_config_cluster.yml delete secret -n $namespace $token
+    done
+  done
 
+  echo "Fixing canal..."
+  for pod in $(kubectl --kubeconfig kube_config_cluster.yml get pods -n kube-system -l k8s-app=canal -o name)
+  do
+    kubectl --kubeconfig kube_config_cluster.yml -n kube-system delete --grace-period=0 --force $pod
+  done
+
+  echo "Fixing coredns..."
+  for pod in $(kubectl --kubeconfig kube_config_cluster.yml get pods -n kube-system -l k8s-app=kube-dns -o name)
+  do
+    kubectl --kubeconfig kube_config_cluster.yml -n kube-system delete --grace-period=0 --force $pod
+  done
+
+  echo "Fixing coredns-autoscaler..."
+  for pod in $(kubectl --kubeconfig kube_config_cluster.yml get pods -n kube-system -l k8s-app=coredns-autoscaler -o name)
+  do
+    kubectl --kubeconfig kube_config_cluster.yml -n kube-system delete --grace-period=0 --force $pod
+  done
+
+  echo "Fixing metrics-server..."
+  for pod in $(kubectl --kubeconfig kube_config_cluster.yml get pods -n kube-system -l k8s-app=metrics-server -o name)
+  do
+    kubectl --kubeconfig kube_config_cluster.yml -n kube-system delete --grace-period=0 --force $pod
+  done
+
+  echo "Fixing rke jobs..."
+  for job in $(kubectl --kubeconfig kube_config_cluster.yml get job -n kube-system -o name | grep rke-)
+  do
+    kubectl --kubeconfig kube_config_cluster.yml -n kube-system delete --grace-period=0 --force $job
+  done
+
+  echo "Fixing nginx-ingress..."
+  for pod in $(kubectl --kubeconfig kube_config_cluster.yml get pods -n ingress-nginx -l app=ingress-nginx -o name)
+  do
+    kubectl --kubeconfig kube_config_cluster.yml -n ingress-nginx delete --grace-period=0 --force $pod
+  done
+
+  echo "Fixing rancher..."
+  for pod in $(kubectl --kubeconfig kube_config_cluster.yml get pods -n cattle-system -l app=rancher -o name)
+  do
+    kubectl --kubeconfig kube_config_cluster.yml -n cattle-system delete --grace-period=0 --force $pod
+  done
+
+  echo "Fixing cattle-node-agent..."
+  for pod in $(kubectl --kubeconfig kube_config_cluster.yml get pods -n cattle-system -l app=cattle-agent -o name)
+  do
+    kubectl --kubeconfig kube_config_cluster.yml -n cattle-system delete --grace-period=0 --force $pod
+  done
+
+  echo "Fixing cattle-cluster-agent..."
+  for pod in $(kubectl --kubeconfig kube_config_cluster.yml get pods -n cattle-system -l app=cattle-cluster-agent -o name)
+  do
+    kubectl --kubeconfig kube_config_cluster.yml -n cattle-system delete --grace-period=0 --force $pod
+  done
+
+  echo "Rolling docker restart..."
+  for node in $(cat cluster.yml | grep ' address:' | awk '{print $3}')
+  do
+    echo "Node: $node"
+    ssh "$SSH_USER"@"$node" "systemctl restart docker"
+    echo "Waiting for etcd is to start..."
+    while ! ssh "$SSH_USER"@"$node" "docker inspect -f '{{.State.Running}}' etcd"
+    do
+      echo "Sleeping..."
+    done
+    echo "Sleeping for 5 seconds before moving to next node"
+    sleep 5
+  done
+
+  echo "Running final rke up..."
+  rke up
+
+  echo "Updating kubeconfig in configmap..."
+DATA="$(cat <<EOF
+"data": {
+  "kube_config_cluster.yml": "$(cat kube_config_cluster.yml)"
+}
+EOF
+)"
+  kubectl -n cattle-rescue patch configmap "$TargetCluster" --type merge -p "$DATA"
+
+  check_cluster_health "$TargetCluster"
+  if [ $? -eq 0 ]
+  then
+    echo "Cluster $TargetCluster is healthy"
+  else
+    echo "Cluster $TargetCluster is unhealthy"
+  fi
+
+echo "Updating configmap for active and preferred cluster in pair map"
+DATA="$(cat <<EOF
+"data": {
+  "active": "$TargetCluster",
+  "preferred": "$TargetCluster"
+}
+EOF
+)"
+  kubectl -n cattle-rescue patch configmap "$pair" --type merge -p "$DATA"
+
+  echo "Failover completed successfully"
+}
+
+function soft_cluster_failover () {
+  DATE=`date +%s`
+  SourceCluster=$1
+  TargetCluster=$2
+  echo "Starting soft failover from $SourceCluster to $TargetCluster"
+  cd /tmp/"$pair"/"$SourceCluster"/
+  echo "Setting SSH access..."
+  SSH_USER=`cat ./ssh-user`
+  unlink /root/.ssh/id_rsa
+  ln -s ./ssh-key /root/.ssh/id_rsa
+  echo "Taking snapshot of $SourceCluster"
+  snapshot_name=`echo CattleRescue-"$DATE"`
+  rke etcd snapshot-save --name "$snapshot_name"
+  echo "Shutting down docker on $SourceCluster"
+  for node in $(cat cluster.yml | grep ' address:' | awk '{print $3}')
+  do
+    ssh "$SSH_USER"@$node "systemctl disable docker; systemctl stop docker"
+  done
+  echo "Calling Flip Provider..."
+  flip_dns $pair $SourceCluster $TargetCluster
+  echo "Bring up target cluster..."
+  bring_up_target_cluster $pair $SourceCluster $TargetCluster $snapshot_name
 }
 
 function hard_cluster_failover () {
