fd27dcd86c53c890074fb7db0b322a6fbd693fa0
Matthew Mattox <cube8021@gmail.com>
2020-03-19 14:30:27 -0500

    Update failover.sh

--- stand-alone/failover.sh
+++ stand-alone/failover.sh
@@ -4,6 +4,7 @@ DATE=`date +%s`
 
 Source=$1
 Target=$2
+S3Backup=$3
 
 read -p "Are you sure you want to failover from $Source to $Target ? " -n 1 -r
 echo    # (optional) move to a new line
@@ -16,6 +17,10 @@ fi
 echo "Taking snapshot of $Source..."
 cd ./"$Source"/
 rke etcd snapshot-save --name Cattle-DR-"$DATE"
+if [[ ! "$S3Backup" == "True" ]]
+then
+  ./pull_snapshots.sh
+fi
 
 echo "Shutting down "$Source"..."
 ./shutdown.sh
@@ -33,4 +38,9 @@ fi
 
 echo "Starting up "$Target "..."
 cd ./"$Target"/
+if [[ ! "$S3Backup" == "True" ]]
+then
+  ./push_snapshots.sh
+fi
+
 ./startup.sh Cattle-DR-"$DATE"
